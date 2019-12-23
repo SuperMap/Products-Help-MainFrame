2196e9d3717acbe9fec132249f2010f70f35ec0e
liuxiaoni <liuxiaoni@supermap.com>
2019-12-23 09:47:22 +0800

    修改产品名称

--- src/components/LayoutHeader/menu.js
+++ src/components/LayoutHeader/menu.js
@@ -4,32 +4,35 @@ import React from 'react';
 
 const menu = (
   <Menu>
-  <Menu.Item key="iDesktop.NET">
-    <a target="_parent" href="http://localhost:8080/iDesktop_NET">SuperMap iDesktop.NET</a>
+  <Menu.Item key="iDesktop">
+    <a target="_parent" href="http://help.supermap.com/iDesktop">SuperMap iDesktop</a>
   </Menu.Item>
-   <Menu.Item key="iDesktop Java">
-    <a target="_parent" href="http://Localhost:8080/iDesktop_Java">SuperMap iDesktop Java</a>
+   <Menu.Item key="iDesktopX">
+    <a target="_parent" href="http://help.supermap.com/iDesktopX">SuperMap iDesktopX</a>
   </Menu.Item>
    <Menu.Item key="iObjects">
-    <a target="_parent" href="http://Localhost:8080/iObjects">SuperMap iObjects</a>
+    <a target="_parent" href="http://help.supermap.com/iObjects">SuperMap iObjects</a>
   </Menu.Item>
   <Menu.Item key="iServer">
-    <a target="_parent" href="http://Localhost:8080/iServer">SuperMap iServer</a>
+    <a target="_parent" href="http://help.supermap.com/iServer">SuperMap iServer</a>
   </Menu.Item>
   <Menu.Item key="iPortal">
-    <a target="_parent" href="http://localhost:8080/iPortal">SuperMap iPortal</a>
+    <a target="_parent" href="http://help.supermap.com/iPortal">SuperMap iPortal</a>
   </Menu.Item>
    <Menu.Item key="iEdge">
-   <a target="_parent" href="http://localhost:8080/iEdge">SuperMap iEdge</a>
+   <a target="_parent" href="http://help.supermap.com/iEdge">SuperMap iEdge</a>
   </Menu.Item>
-  {/* <Menu.Item key="iManager">
-    <a target="_parent" href="http://localhost:8080/iManager">SuperMap iManager</a>
+  <Menu.Item key="iManager">
+    <a target="_parent" href="http://help.supermap.com/iManager">SuperMap iManager</a>
   </Menu.Item>
-  <Menu.Item key="iClient">
-    <a target="_parent" href="http://localhost:8080/iClient">SuperMap iClient</a>
+  <Menu.Item key="iManager K8S">
+    <a target="_parent" href="http://help.supermap.com/iManager_K8S">SuperMap iManager K8S</a>
+  </Menu.Item>
+  {/* <Menu.Item key="iClient">
+    <a target="_parent" href="http://help.supermap.com/iClient">SuperMap iClient</a>
   </Menu.Item> */}
   <Menu.Item key="iMobile">
-    <a target="_parent" href="http://localhost:8080/iMobile">SuperMap iMobile</a>
+    <a target="_parent" href="http://help.supermap.com/iMobile">SuperMap iMobile</a>
   </Menu.Item>
   </Menu>
   );
